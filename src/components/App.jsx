af3f6d312ee65bca9e8aaa6005d8898fde8f8000
yzwang-13 <53463432+yzwang-13@users.noreply.github.com>
2019-12-12 22:30:49 +1100

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const state = useState(0);
+  // const rgb = [9, 132, 277];
+  // destructing
+  const [count, setCount] = useState(0);
+  const [red, green, blue] = [9, 132, 277];
+
+  console.log(state);
+
+  function add() {
+    setCount(count + 1);
+  }
+
+  function substract() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={add}>+</button>
+      <button onClick={substract}>-</button>
+    </div>
+  );
 }
 
 export default App;
